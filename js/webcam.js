2540063663b4d399408ebd69554ecbb5ca8e6514
Itsick <any.site.n.site@gmail.com>
2018-04-27 16:06:17 +0300

    add css

--- js/webcam.js
+++ js/webcam.js
@@ -2,37 +2,44 @@ localstream = null;
 cancelXhr = false;
 captureButton = document.getElementById('capture');
 player = document.getElementById('vid');
-canvas = document.getElementById('canvas');
-context = canvas.getContext('2d');
-if (navigator.webkitGetUserMedia!=null) {
+player = null;
+textExampleInput = document.getElementById('text-example');
+imgExample = document.getElementById('example');
+h2 = document.getElementById('h2');
+h2.innerText = chrome.i18n.getMessage("h2_title");
+document.getElementsByTagName('title').innerText = chrome.i18n.getMessage("h2_title");
+console.log(chrome.i18n.getMessage("capture_button_text"));
+$('#capture').text(chrome.i18n.getMessage("capture_button_text"));
+var canvas = document.getElementById('canvas'),
+  context = canvas.getContext('2d');
 
-    var options = { 
-        video:true, 
-        audio:false 
-    };  
-    navigator.webkitGetUserMedia(options, 
-        function(stream) { 
-            vid.src = window.webkitURL.createObjectURL(stream);
-            localstream = stream;
-            vid.play();
-            console.log("streaming");
-        }, 
-        function(e) { 
-        console.log("background error : " + e);
-        }); 
-} 
+GumHelper.startVideoStreaming(function callback(err, stream, videoElement, width, height) {
+  if(err) {
+    errorDiv = document.getElementById('error');
+    errorDiv.innerText = chrome.i18n.getMessage("error");
+    errorDiv.classList.add('visible');
+  } else {
+     
+      player = videoElement;
+      videoElement.id = 'vid';
+      jQuery('.video-wrp-inner').prepend(videoElement);
+      jQuery('.text-example-wrp').width(player.videoWidth - 60 )
+      
+      // (or you could just keep a reference and use it later)
+  }
+}, { timeout: 20000 });
  captureButton.addEventListener('click', () => {
     // Draw the video frame to the canvas.
-    context.drawImage(player, 0, 0, canvas.width, canvas.height);
+    canvas.width = player.videoWidth;
+    canvas.height = player.videoHeight;
+    context.drawImage(player, 0, 0, player.videoWidth, player.videoHeight);
     canvasToDataURL = canvas.toDataURL();
-    $('#topText').html('uploading <a href="#" id=a_cancel>Cancel</a>').find('#a_cancel').click(function(){
-        cancelXhr = true;
-    });
+    
 
 			
 			//var url= screenshot.url;
-			var options= localStorage['options'];
-    hr = $.ajax({
+	var options= localStorage['options'],
+        hr = $.ajax({
         url:'https://www.openscreenshot.com/upload3.asp',
         type:'post',
         data:{
@@ -44,28 +51,31 @@ if (navigator.webkitGetUserMedia!=null) {
             data:canvasToDataURL
             //service:service
             },
-            complete: function (a,b,c) {
-                            if(cancelXhr) {
-                                $('#topText').html('Canceled!'); return;
-                            }    
-                            /* Fixing the vulnerable that discovered by Google */
-                            var response = a.responseText.replace(/^\s+|\s+$/g,"");  // remove trailing white space
-                            if (/"/.test(response) || />/.test(response) || /</.test(response) || /'/.test(response) || response.indexOf("http:") != 0 ) {
-                                $("#topText").html('Please try again in some minutes. We are working to solve this issue.');
-                            } else {
-                                if(cancelXhr) {
-                                    return;
-                                }
-                                response = response.split(',');
-                                imageURL = response[0];
-                                onlineUrl = imageURL;
-                                imageDelete = response[1];
-                                chrome.runtime.sendMessage({action: "imageUrlCreated",url:imageURL});
-                                
-                            }
-       
-                            }
-                        })
-    
+        complete: function (a,b,c) {
+            if(cancelXhr) {
+                $('#topText').text(chrome.i18n.getMessage("canceled")); return;
+            }    
+            /* Fixing the vulnerable that discovered by Google */
+            var response = a.responseText.replace(/^\s+|\s+$/g,"");  // remove trailing white space
+            if (/"/.test(response) || />/.test(response) || /</.test(response) || /'/.test(response) || response.indexOf("http:") != 0 ) {
+                $("#topText").html('Please try again in some minutes. We are working to solve this issue.');
+            } else {
+                if(cancelXhr) {
+                    return;
+                }
+                response = response.split(',');
+                imageURL = response[0];
+                onlineUrl = imageURL;
+                imageDelete = response[1];
+                chrome.runtime.sendMessage({action: "imageUrlCreated",url:imageURL});
+                
+            }
+
+            }
+        })
+        $('#topText').html(chrome.i18n.getMessage("uploading") + '<a href="#" id=a_cancel>' + chrome.i18n.getMessage("cancel") + '</a>').find('#a_cancel').click(function(){
+            cancelXhr = true;
+            hr.abort()
+        });
 
   });
